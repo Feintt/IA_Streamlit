dd928b625f732e94a1b1ad9c0f1dfc95fc8137be
Feintt <0253214@up.edu.mx>
2024-02-21 21:04:55 -0600

    refactor code

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,5 +1,3 @@
-import time
-import pandas as pd
 
 
 def execution_time(func):
@@ -8,6 +6,7 @@ def execution_time(func):
     """
 
     def wrapper(*args, **kwargs):
+        import time  # Lazy import
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
@@ -17,10 +16,99 @@ def execution_time(func):
     return wrapper
 
 
-def extract_nodes_and_edges_from_csv(file_path):
+def extract_nodes_and_edges_from_csv(nodes_path="data/nodes.csv", edges_path="data/edges_with_weights.csv"):
     """
     Extracts the nodes and edges from a CSV file and returns them as lists.
     """
-    nodes = pd.read_csv(file_path + "nodes.csv")
-    edges = pd.read_csv(file_path + "edges.csv")
+    import pandas as pd  # Lazy import
+    nodes = pd.read_csv(nodes_path)['Node'].tolist()
+    edges = pd.read_csv(edges_path)
     return nodes, edges
+
+
+def generate_graph(nodes, edges):
+    """
+    Generates a graph with NetworkX.
+    """
+    import networkx as nx  # Lazy import
+    graph = nx.DiGraph()
+    graph.add_nodes_from(nodes)
+    graph.add_weighted_edges_from(
+        [(row['Origin'], row['Destination'], row['Weight']) for index, row in edges.iterrows()])
+    return graph
+
+
+def add_edges_to_plotly_graph(fig, edge_x, edge_y):
+    """
+    Adds the edges to the Plotly graph.
+    :param fig:
+    :param edge_x:
+    :param edge_y:
+    :return:
+    """
+    import plotly.graph_objects as go  # Lazy import
+    fig.add_trace(go.Scatter(x=edge_x,
+                             y=edge_y,
+                             mode='lines',
+                             line=dict(width=2, color='grey'),
+                             hoverinfo='none'))
+
+
+def add_color_to_nodes(fig, node_x, node_y, node_degree, graph):
+    """
+    Adds color to the nodes based on their degree to the Plotly graph.
+    :param fig:
+    :param graph:
+    :param node_x:
+    :param node_y:
+    :param node_degree:
+    :return:
+    """
+    import plotly.graph_objects as go  # Lazy import
+    fig.add_trace(go.Scatter(x=node_x,
+                             y=node_y,
+                             mode='markers+text',
+                             text=[node for node in graph.nodes()],
+                             textposition="bottom center",
+                             marker=dict(size=[(deg + 1) * 5 for deg in node_degree],
+                                         color=node_degree, colorscale='Blues',
+                                         showscale=True, colorbar=dict(title='Node Degree')),
+                             hoverinfo='text',
+                             hovertext=[f'{node}<br>Degree: {deg}' for node, deg in zip(graph.nodes(), node_degree)]))
+
+
+def highlight_path(path, pos, fig):
+    """
+    Highlights the path in the graph by changing the color of the edges and nodes.
+    """
+    import plotly.graph_objects as go  # Lazy import
+    path_edges = zip(path, path[1:])
+    path_x = []
+    path_y = []
+    for edge in path_edges:
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        path_x.extend([x0, x1, None])
+        path_y.extend([y0, y1, None])
+    fig.add_trace(go.Scatter(x=path_x,
+                             y=path_y,
+                             mode='lines',
+                             line=dict(color='firebrick', width=3),
+                             hoverinfo='none'))
+
+
+def beautify_graph(fig):
+    """
+    Beautifies the graph by adding edges and nodes with a color scale based on their degree.
+    """
+    fig.update_layout(title_text='Interactive Network Graph',
+                      title_x=0.5,
+                      title_font_size=24,
+                      showlegend=False,
+                      hovermode='closest',
+                      margin=dict(b=20, l=20, r=20, t=40),
+                      xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                      yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                      paper_bgcolor='white',
+                      plot_bgcolor='white',
+                      font=dict(size=12, color='black'))
--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import streamlit as st
 import plotly.graph_objects as go
 from helpers.algorithms import bfs_algorithm
+from helpers import *
 
 # Read the CSV of nodes
-nodes = pd.read_csv("data/nodes.csv")
-edges_with_weights = pd.read_csv("data/edges_with_weights.csv")
-nodes_list = nodes['Node'].tolist()
+nodes, edges_with_weights = extract_nodes_and_edges_from_csv()
 
 # Create a simple graph with NetworkX
-Graph = nx.DiGraph()
-Graph.add_nodes_from(nodes_list)
-Graph.add_weighted_edges_from(
-    [(row['Origin'], row['Destination'], row['Weight']) for index, row in edges_with_weights.iterrows()])
+Graph = generate_graph(nodes, edges_with_weights)
 
 # Generate a fixed layout for the nodes
 pos = nx.kamada_kawai_layout(Graph)
@@ -30,8 +25,8 @@ st.pyplot(plt)
 # Streamlit configuration
 st.title("Select nodes to find a path")
 
-start_node = st.selectbox('Start node:', nodes_list)
-target_node = st.selectbox('Target node:', nodes_list)
+start_node = st.selectbox('Start node:', nodes)
+target_node = st.selectbox('Target node:', nodes)
 path = bfs_algorithm(Graph, start_node, target_node)
 
 # Prepare data for Plotly graph
@@ -50,43 +45,15 @@ node_degree = [Graph.degree(node) for node in Graph.nodes()]
 # Create a figure
 fig = go.Figure()
 
-# Add edges as lines with improved styling
-fig.add_trace(go.Scatter(x=edge_x, y=edge_y, mode='lines', line=dict(width=2, color='grey'), hoverinfo='none'))
-
-# Add nodes with a color scale based on their degree
-fig.add_trace(go.Scatter(x=node_x, y=node_y, mode='markers+text', text=[node for node in Graph.nodes()],
-                         textposition="bottom center",
-                         marker=dict(size=[(deg + 1) * 5 for deg in node_degree], color=node_degree, colorscale='Blues',
-                                     showscale=True, colorbar=dict(title='Node Degree')),
-                         hoverinfo='text',
-                         hovertext=[f'{node}<br>Degree: {deg}' for node, deg in zip(Graph.nodes(), node_degree)]))
+add_edges_to_plotly_graph(fig, edge_x, edge_y)
+add_color_to_nodes(fig, node_x, node_y, node_degree, Graph)
 
 # Highlight the found path, if it exists, with better visibility
 if path != -1:
-    path_edges = zip(path, path[1:])
-    path_x = []
-    path_y = []
-    for edge in path_edges:
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-        path_x.extend([x0, x1, None])
-        path_y.extend([y0, y1, None])
-    fig.add_trace(go.Scatter(x=path_x, y=path_y, mode='lines', line=dict(color='firebrick', width=3), hoverinfo='none'))
-
-# Update the layout for a more polished look
-fig.update_layout(title_text='Interactive Network Graph', title_x=0.5, title_font_size=24,
-                  showlegend=False, hovermode='closest',
-                  margin=dict(b=20, l=20, r=20, t=40),
-                  xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                  yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                  paper_bgcolor='white', plot_bgcolor='white',
-                  font=dict(size=12, color='black'))
-
-# Display the updated figure in Streamlit
-st.plotly_chart(fig, use_container_width=True)
-
-# Show information about the found path
-if path != -1:
+    highlight_path(path, pos, fig)
     st.write(f"Path found from {start_node} to {target_node}: {' -> '.join(path)}")
 else:
     st.write("No path found.")
+
+beautify_graph(fig)
+st.plotly_chart(fig, use_container_width=True)
